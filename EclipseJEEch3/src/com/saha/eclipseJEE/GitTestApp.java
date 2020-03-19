87b33ba313b51d41bcccfa4ff2b8ceaaa0483799
admin <sahabs500@yahoo.com>
2020-03-20 01:58:59 +0530

    Updated GitTestApp

--- EclipseJEEch3/src/com/saha/eclipseJEE/GitTestApp.java
+++ EclipseJEEch3/src/com/saha/eclipseJEE/GitTestApp.java
@@ -3,7 +3,8 @@ package com.saha.eclipseJEE;
 public class GitTestApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("Git practice in local repo in EclipseJEEch3");
 
 	}
 
